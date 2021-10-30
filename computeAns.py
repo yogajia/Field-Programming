19891a9fff4cf66c60a4a5d9b1bd03f8d06f884b
JoshuaYu-crash <1239299797@qq.com>
2021-10-30 11:14:07 +0800

    fix bug

--- computeAns.py
+++ computeAns.py
@@ -1,67 +1,75 @@
-# input:
-# num: a list of Integer or Decimal
-# symbol: a list of (+, - , *, /)
-
-# output:
-# ans: the result of the calculation
-
-def computeAns(num, symbol):
-    for i in range(len(num)):
-        if len(symbol)!=0:
-            if symbol[i] == '*' or symbol[i] == '/':
-                if symbol[i] == '*':
-                    ans = num[i] * num[i + 1]
-                elif symbol[i] == '/':
-                    ans = num[i] / num[i + 1]
-                num.insert(i, ans)
-                num.pop(i + 1)
-                num.pop(i + 1)
-                symbol.pop(i)
-                print(i)
-                print(num)
-                print(symbol)
-
-    ans = num[0]
-    for i in range(len(symbol)):
-        if symbol[i] == '+':
-            ans += num[i + 1]
-        elif symbol[i] == '-':
-            ans -= num[i + 1]
-
-    return ans
+import re
+import functools
 
 
-def computeAns2(num, symbol):
-    if symbol[0] == '+':
-        ans = num[0] + num[1]
-    elif symbol[0] == '-':
-        ans = num[0] - num[1]
-    elif symbol[0] == '*':
-        ans = num[0] * num[1]
-    else:
-        ans = num[0] / num[1]
-    return ans
-# num1 = [100, 200, 300, 400]
-# num2 = [2.2, 200, 123, 300]
-# num3 = [5, 2, 3, 1]
-#
-# symbol1 = ['+', '-', '*']
-# symbol2 = ['-', '-', '-']
-# symbol3 = ['/', '/', '/']
-#
-# ans1 = num1[0] + num1[1] - num1[2] * num1[3]
-# temp_ans1 = computeAns(num1, symbol1)
-# print(temp_ans1, ans1)
-# print(temp_ans1 == ans1)
-#
-# ans2 = num2[0] - num2[1] - num2[2] - num2[3]
-# temp_ans2 = computeAns(num2, symbol2)
-# print(temp_ans2, ans2)
-# print(temp_ans2 == ans2)
-#
-# ans3 = num3[0] / num3[1] / num3[2] / num3[3]
-# temp_ans3 = computeAns(num3, symbol3)
-# print(temp_ans3, ans3)
-# print(temp_ans3 == ans3)
+def formatInput(formula):
+    """标准化输入表达式，去除多余空格等"""
+    formula = formula.replace(' ', '')
+    formula = formula.replace('++', '+')
+    formula = formula.replace('+-', '-')
+    formula = formula.replace('-+', '-')
+    formula = formula.replace('--', '+')
+    return formula
 
 
+def mul_divOperation(s):
+    """乘法除法运算"""
+    s = formatInput(s)
+    sub_str = re.search('(\d+\.?\d*[*/]-?\d+\.?\d*)', s)
+    while sub_str:
+        sub_str = sub_str.group()
+        if sub_str.count('*'):
+            l_num, r_num = sub_str.split('*')
+            s = s.replace(sub_str, str(float(l_num) * float(r_num)))
+        else:
+            l_num, r_num = sub_str.split('/')
+            s = s.replace(sub_str, str(float(l_num) / float(r_num)))
+
+        s = formatInput(s)
+        sub_str = re.search('(\d+\.?\d*[*/]\d+\.?\d*)', s)
+
+    return s
+
+
+def add_minusOperation(s):
+    """加法减法运算
+    思路：在最前面加上+号，然后正则匹配累加
+    """
+    s = formatInput(s)
+    s = '+' + s
+    tmp = re.findall('[+\-]\d+\.?\d*', s)
+    s = str(functools.reduce(lambda x, y: float(x) + float(y), tmp))
+    return s
+
+
+def compute(formula):
+    """无括号表达式解析"""
+    # ret = formula[1:-1]
+    ret = formatInput(formula)
+    ret = mul_divOperation(ret)
+    ret = add_minusOperation(ret)
+    return ret
+
+
+def computeAns(formula):
+    """计算程序入口"""
+    has_parenthesise = formula.count('(')
+    formula = formatInput(formula)
+    while has_parenthesise:
+        sub_parenthesise = re.search('\([^()]*\)', formula)  # 匹配最内层括号
+        if sub_parenthesise:
+            formula = formula.replace(sub_parenthesise.group(), compute(sub_parenthesise.group()[1:-1]))
+        else:
+            has_parenthesise = False
+
+    ret = compute(formula)
+    return ret
+
+
+if __name__ == '__main__':
+    # print(mul_divOperation('17.0*7/3.0*3+1/2'))
+    # print(add_minusOperation('1-2-3+1-4'))
+    # print(calc('(9+8)*7/(5-2)*3+1/2'))
+    s = "99+(1+2=(8)-(7+9))"
+
+    print(calc("(1+2) - 3+4"))
--- generateProblem.py
+++ generateProblem.py
@@ -1,37 +1,50 @@
 from generateNum import generateNum
 from generateSymbol import generateSymbol
 import random
-from computeAns import computeAns2
+from computeAns import computeAns
 
-num = 2
 
 def generateProblem(level):
+    num = random.randint(2, 4)
     if level == 1:
-        list1 = generateNum( num, False, 100)
+        list1 = generateNum(num, False, 100)
         list2 = generateSymbol(num, False)
-        problem = fix(list1,list2)
-        result = computeAns2(list1,list2)
+        problem = fix(list1, list2)
+        result = float(computeAns(problem))
 
     if level == 2:
-        list1 = generateNum( num, False, 10000)
+        list1 = generateNum(num, False, 10000)
         list2 = generateSymbol(num, True)
-        problem = fix(list1,list2)
-        result = computeAns2(list1,list2)
+        problem = fix(list1, list2)
+        result = float(computeAns(problem))
 
     if level == 3:
-        list1 = generateNum( num, True, 10000)
+        list1 = generateNum(num, True, 10000)
         list2 = generateSymbol(num, True)
-        problem = fix(list1,list2)
-        result = computeAns2(list1,list2)
+        problem = fix(list1, list2)
+        result = float(computeAns(problem))
 
     return (problem, result)
 
-def fix(list1, list2):
-    lens = len(list1)
-    ans = ""
-    for i in range(lens):
-        ans = ans + str(list1[i])
-        if i < lens -1:
-            ans = ans + list2[i]
 
-    return ans
+def fix(num, symbol):
+    numLen = len(num)
+    if numLen < 4:
+        ans = ""
+        for i in range(numLen):
+            ans = ans + str(num[i])
+            if i < numLen - 1:
+                ans = ans + symbol[i]
+        return ans
+    else:
+        pos = random.randint(0, 2)
+        ans = ""
+        for i in range(numLen):
+            if i == pos:
+                ans += "("
+            ans += str(num[i])
+            if i == pos + 1:
+                ans += ")"
+            if i < numLen - 1:
+                ans = ans + symbol[i]
+        return ans
